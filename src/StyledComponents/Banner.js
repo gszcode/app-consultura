9c8574989276610ac4a92f151c849b5b816d75d9
Gabriel <gszcode@gmail.com>
2023-06-09 23:32:18 -0300

    estilos al div de corte

--- src/StyledComponents/Banner.js
+++ src/StyledComponents/Banner.js
@@ -15,8 +15,16 @@ export const SubTitle = styled.h3`
   font-weight: bold;
   font-size: 18px;
 `
-
 export const TitleBanner = styled.h2`
   font-weight: bold;
   font-size: 35px;
+`
+export const ImgContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  clip-path: polygon(0 100%, 100% 100%, 100% 90%, 0 98%);
+  background-color: ${colors.primary};
 `
