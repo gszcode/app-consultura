bd0194aaf5823bfd442531c236927dff97159a88
Gabriel <gszcode@gmail.com>
2023-06-09 22:48:31 -0300

    pagina Home agregada a la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import './index.css'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   )
 }
