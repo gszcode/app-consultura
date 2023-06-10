4d517fa5d953bba9da65fea69ff0b0fb3fe3794d
Gabriel <gszcode@gmail.com>
2023-06-09 22:46:43 -0300

    componente banner para home creado

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import { Section, SubTitle, TitleBanner } from '../StyledComponents/Banner'
+import { Button, IconBtn } from '../StyledComponents/Button'
+
+const Banner = () => {
+  return (
+    <Section>
+      <SubTitle>You make the request, we make the connection</SubTitle>
+      <TitleBanner>
+        Somos una consultora de servicios IT especializada en reclutamiento
+      </TitleBanner>
+      <Button>
+        SABER MÁS <IconBtn className="fa-solid fa-arrow-down" />
+      </Button>
+    </Section>
+  )
+}
+
+export default Banner
