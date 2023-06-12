60e37b8fbb0917f42fc30a54797db2ce34dcd4e2
Gabriel <gszcode@gmail.com>
2023-06-11 23:13:39 -0300

    estilos reutilizables para los titulos

---
+++ src/StyledComponents/Titles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { colors } from '../theme'
+
+export const SubTitle = styled.h3`
+  color: ${colors.third};
+  font-weight: bold;
+  font-size: 18px;
+`
+export const Title = styled.h2`
+  font-weight: bold;
+  font-size: 30px;
+`
