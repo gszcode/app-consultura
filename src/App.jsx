4957ed28962c40dfebea8100fc2158f3f68745e4
Gabriel <gszcode@gmail.com>
2023-06-12 22:43:32 -0300

    landing agregada a la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import './index.css'
 import Header from './components/Header'
-import Home from './pages/Home'
-import About from './pages/About'
-import Hiring from './pages/Hiring'
-import Contact from './pages/Contact'
 import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import { Index } from './pages/Index'
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
-      <About />
-      <Hiring />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
       <Footer />
     </>
   )
