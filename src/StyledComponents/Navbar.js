5354ef10db0c1b3090e15daa507b1a663716eaeb
Gabriel <gszcode@gmail.com>
2023-06-12 23:24:56 -0300

    estilos al menu de escritorio realizados

--- src/StyledComponents/Navbar.js
+++ src/StyledComponents/Navbar.js
@@ -32,6 +32,10 @@ export const Icono = styled.i`
   &:hover {
     color: ${colors.second};
   }
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
 `
 export const Menu = styled.ul`
   width: 100%;
@@ -45,9 +49,32 @@ export const Menu = styled.ul`
   padding: 0 20px;
   box-shadow: 0px 1px 5px 0px rgba(84, 84, 84, 1);
   z-index: 10;
-`
 
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`
 export const Item = styled(Link)`
+  color: ${colors.white};
+  font-weight: 200;
+  font-size: 18px;
+  cursor: pointer;
+
+  &:hover {
+    color: ${colors.second};
+  }
+`
+export const MenuLg = styled.ul`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+  }
+`
+export const ItemLg = styled(Link)`
   color: ${colors.white};
   font-weight: 200;
   font-size: 18px;
