dfc3bb1e765926a6bcc8c2229370816c0d1e42c9
Gabriel <gszcode@gmail.com>
2023-06-12 22:44:23 -0300

    index/home creado

---
+++ src/pages/Index.jsx
@@ -0,0 +1,15 @@
+import Home from '../components/Home'
+import About from '../components/About'
+import Hiring from '../components/Hiring'
+import Contact from '../components/Contact'
+
+export function Index() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Hiring />
+      <Contact />
+    </>
+  )
+}
