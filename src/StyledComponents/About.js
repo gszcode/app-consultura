324571b0b7ffb7b1ed4abd1450ea429c42db6819
Gabriel <gszcode@gmail.com>
2023-06-28 02:18:28 -0300

    nuevos estilos de componente sobre nosotros

--- src/StyledComponents/About.js
+++ src/StyledComponents/About.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { colors } from '../theme'
+import { Link } from 'react-router-dom'
 
 export const SectionAbout = styled.section`
   width: 100%;
@@ -69,5 +70,180 @@ export const ImageAbout = styled.div`
 export const Image = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
+`
+
+// About Page
+export const Container = styled.section`
+  width: 100%;
+`
+export const ContainerTitle = styled.section`
+  width: 100%;
+  height: 235px;
+  line-height: 200px;
+  font-size: 30px;
+  margin: 25px 0;
+  padding: 0 20px;
+
+  @media (min-width: 500px) {
+    height: 400px;
+    line-height: 380px;
+    font-size: 40px;
+    margin-left: 30px;
+  }
+`
+export const ImgContainer = styled.div`
+  position: absolute;
+  top: -580px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  clip-path: polygon(45px 100%, 100% 100%, 100% 92%, 120px 98%);
+  background-color: ${colors.primary};
+  padding: 0 20px;
+
+  @media (min-width: 500px) {
+    top: -400px;
+    clip-path: polygon(0 100%, 100% 100%, 100% 86%, 0 98%);
+  }
+
+  @media (min-width: 900px) {
+    clip-path: polygon(200px 100%, 100% 100%, 100% 80%, 275px 98%);
+  }
+`
+export const DivText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0;
+  padding: 0 20px;
+
+  @media (min-width: 500px) {
+    padding: 30px;
+  }
+
+  @media (min-width: 900px) {
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 100px;
+  }
+`
+export const TextSubTitle = styled.h2`
+  font-size: 22px;
+  margin: 20px 0;
+
+  @media (min-width: 500px) {
+    font-size: 25px;
+  }
+
+  @media (min-width: 900px) {
+    font-size: 30px;
+  }
+`
+export const TextParagraph = styled.p`
+  font-size: 16px;
+  color: rgb(118, 120, 127);
+  letter-spacing: 1px;
+
+  @media (min-width: 500px) {
+    font-size: 20px;
+  }
+
+  @media (min-width: 900px) {
+    font-size: 22px;
+  }
+`
+export const AboutImage = styled.img`
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+`
+export const DivData = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0;
+  background-color: #000;
+  padding: 20px;
+
+  @media (min-width: 500px) {
+    padding: 30px;
+  }
+
+  @media (min-width: 900px) {
+    padding: 100px;
+  }
+`
+export const TextSubTitle2 = styled.h2`
+  font-size: 22px;
+  margin: 20px 0;
+  color: rgb(41, 81, 213);
+
+  @media (min-width: 500px) {
+    font-size: 25px;
+  }
+
+  @media (min-width: 900px) {
+    font-size: 30px;
+  }
+`
+export const TextParagraph2 = styled.p`
+  font-size: 16px;
+  color: rgb(118, 120, 127);
+  letter-spacing: 1px;
+
+  @media (min-width: 500px) {
+    font-size: 20px;
+  }
+
+  @media (min-width: 900px) {
+    font-size: 22px;
+  }
+`
+export const ButtonLink = styled(Link)`
+  display: block;
+  background-color: ${colors.white};
+  border: 1px solid transparent;
+  color: ${colors.third};
+  font-weight: 500;
+  width: 190px;
+  height: 40px;
+  text-align: center;
+  font-size: 14px;
+  line-height: 39px;
+  letter-spacing: 1px;
+  margin: 15px 0;
+
+  &:hover {
+    background-color: transparent;
+    border: 1px solid ${colors.third};
+  }
+`
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0;
+  padding: 20px;
+
+  @media (min-width: 500px) {
+    padding: 30px;
+  }
+
+  @media (min-width: 900px) {
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 100px;
+  }
+`
+export const Img = styled.img`
+  width: 300px;
+  height: 300px;
   object-fit: cover;
 `
